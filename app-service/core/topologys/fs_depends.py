2fc1f743d4b3e46e3b171e304da25f5b2f365373
OlegLebedevRU <olebedev74@gmail.com>
2025-09-18 00:18:43 +0300

    fix x-correlation-id (from paho mqtt)

--- app-service/core/topologys/fs_depends.py
+++ app-service/core/topologys/fs_depends.py
@@ -12,16 +12,23 @@ async def sn_getter_dep(msg: RabbitMessage) -> str:
 async def corr_id_getter_dep(msg: RabbitMessage) -> str | None:
     try:
         if msg.correlation_id:
-            # logging.info("Raw corr_id = %s", msg.correlation_id)
+            logging.info("Raw corr_id = %s", msg.correlation_id)
             corr_id = uuid.UUID(msg.correlation_id)
             logging.info("Received msg.correlation_id = %s", corr_id)
-        elif hasattr(msg.raw_message.headers, "x-correlation-id"):
-            corr_id = uuid.UUID(bytes=msg.raw_message.headers["x-correlation-id"])
-            logging.info("Received headers.x-correlation-id =  %s", corr_id)
-            if corr_id == settings.task_proc_cfg.zero_corr_id:
-                corr_id = None
         else:
             corr_id = None
+        logging.info("Received headers =  %s", msg.raw_message.headers)
+        if "x-correlation-id" in msg.raw_message.headers:
+            corr_id = uuid.UUID(
+                bytes=(msg.raw_message.headers["x-correlation-id"].encode())
+            )
+            logging.info("Received headers.x-correlation-id =  %s", corr_id)
+        else:
+            if corr_id is None:
+                pass
+        if corr_id == settings.task_proc_cfg.zero_corr_id:
+            logging.info("settings.task_proc_cfg.zero_corr_id =  %s", corr_id)
+
     except (TypeError, ValueError, KeyError) as e:
         logging.info("Received from device no corr data, exception = %s", e)
         corr_id = None
