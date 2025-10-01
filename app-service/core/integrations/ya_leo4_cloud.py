065f714d5cc9b9d191de9911032dd4f1d2df9d4e
OlegLebedevRU <olebedev74@gmail.com>
2025-10-01 16:14:47 +0300

    compose.yaml update, nginx.conf update with jwt on, cert validate on

--- app-service/core/integrations/ya_leo4_cloud.py
+++ app-service/core/integrations/ya_leo4_cloud.py
@@ -6,9 +6,21 @@ from core import settings
 log = logging.getLogger(__name__)
 
 
+async def get_token(api_key):
+    r = httpx.get(
+        url=str(settings.leo4.url) + "account/login2",
+        headers={"Authorization": "Bearer " + api_key},
+    )
+    log.info("login to leo4 cloud token %s = ", str(r.json()))
+
+    if r is None:
+        raise HTTPException(status_code=404, detail="Key not found")
+    return r.json()
+
+
 async def get_factory_device_list(api_key):
     r = httpx.get(
-        url=str(settings.leo4.url) + "/account/login2",
+        url=str(settings.leo4.url) + "account/login2",
         headers={"Authorization": "Bearer " + api_key},
     )
     log.info("login to leo4 cloud token %s = ", str(r.json()))
@@ -16,10 +28,17 @@ async def get_factory_device_list(api_key):
     if r is None:
         raise HTTPException(status_code=404, detail="Item not found")
     r1 = httpx.get(
-        url=str(settings.leo4.url) + "/device/list",
+        url=str(settings.leo4.url) + "device/list",
         headers={"Authorization": "Bearer " + r.json()["accessToken"]},
     )
     # -------------------------------------------------
+    if r1.status_code == 403:
+        raise HTTPException(status_code=403, detail="Leo4 cloud auth error")
+    if r1.status_code == 500:
+        log.info("error from cloud device list %s = ", r1.json())
+        raise HTTPException(status_code=500, detail="Leo4 cloud 500 error")
+
     da = r1.json()
-    log.info("from cloud device list %s = ", str(da[0]))
+    if da:
+        log.info("from cloud device list %s = ", str(da[0]))
     return da
