b2393ce69578a0a597df1821839347b2493c59c2
OlegLebedevRU <olebedev74@gmail.com>
2025-10-08 02:22:16 +0300

    nginx conf add locations

--- app-service/core/integrations/ya_leo4_cloud.py
+++ app-service/core/integrations/ya_leo4_cloud.py
@@ -8,7 +8,7 @@ log = logging.getLogger(__name__)
 
 async def get_token(api_key):
     r = httpx.get(
-        url=str(settings.leo4.url) + "account/login2",
+        url=str(settings.leo4.url) + "/api/login/",
         headers={"Authorization": "Bearer " + api_key},
     )
     log.info("login to leo4 cloud token %s = ", str(r.json()))
@@ -20,7 +20,7 @@ async def get_token(api_key):
 
 async def get_factory_device_list(api_key):
     r = httpx.get(
-        url=str(settings.leo4.url) + "account/login2",
+        url=str(settings.leo4.url) + "/api/login/",
         headers={"Authorization": "Bearer " + api_key},
     )
     log.info("login to leo4 cloud token %s = ", str(r.json()))
@@ -28,8 +28,8 @@ async def get_factory_device_list(api_key):
     if r is None:
         raise HTTPException(status_code=404, detail="Item not found")
     r1 = httpx.get(
-        url=str(settings.leo4.url) + "device/list",
-        headers={"Authorization": "Bearer " + r.json()["accessToken"]},
+        url=str(settings.leo4.url) + "/device/list/",
+        headers={"accessToken": r.json()["accessToken"]},
     )
     # -------------------------------------------------
     if r1.status_code == 403:
