22f7d6ecf861523e38588b3e794a0e562d78c74b
OlegLebedevRU <olebedev74@gmail.com>
2025-10-10 13:58:26 +0300

    refactoring log, probe 2

--- app-service/core/config.py
+++ app-service/core/config.py
@@ -170,3 +170,11 @@ def settn_get():
 
 
 settings = settn_get()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=settings.logging.log_format,
+    datefmt=settings.logging.date_format,
+   # filename="/var/log/app/broker.log",
+    filemode="w",
+)
--- app-service/core/fs_broker.py
+++ app-service/core/fs_broker.py
@@ -5,20 +5,18 @@ import logging
 from faststream.rabbit.fastapi import RabbitRouter
 from core.config import settings
 
-logging.basicConfig(
-    level=logging.WARNING,
-    format=settings.logging.log_format,
-    datefmt=settings.logging.date_format,
-    filename="/var/log/app/broker.log",
-    filemode="w",
-)
+
 log = logging.getLogger(__name__)
+fh = logging.FileHandler("/var/log/app/broker.log")
+fh.setLevel(logging.WARNING)
+log.addHandler(fh)
+
 fs_router = RabbitRouter(
     str(settings.faststream.url),
     include_in_schema=False,
     logger=log,
-    # log_level=logging.WARNING,
-    # log_fmt=settings.logging.log_format,
+    #log_level=logging.WARNING,
+    log_fmt=settings.logging.log_format,
     max_consumers=settings.faststream.max_consumers,
 )
 
